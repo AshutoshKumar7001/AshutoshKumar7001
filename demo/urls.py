963667d1d7ebf37a43aa0f26c28846f7e8dff3ec
ASHUTOSH KUMAR <ASHUTOSHKUMAR7001@GMAIL.COM>
2023-08-22 17:57:40 +0530

    second commit

--- demo/urls.py
+++ demo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
+from .views import Another
 
 urlpatterns = [
     path('', views.home),
-    path('p1/', views.page1)
+    path('p1/', views.page1),
+    path('p2/', Another.as_view())
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from .models import Book
 
 # Create your views here.
+
+class Another(View):
+
+    books = Book.objects.all()
+
+    # output = f"We have {len(book)} that many books in DB."
+    output = ''
+
+    for book in books:
+        output += f"We have {book.title} that many books in DB.<br>"
+
+    def get(self, request):
+        return HttpResponse(self.output)
+
+
+    # def get(self, request):
+    #     return HttpResponse('This is the insideclass funtion inside the class demo.')
+
+
 def home(request):
     return HttpResponse('First message from views')
 
